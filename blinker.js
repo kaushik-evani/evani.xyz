9d7ab3b5e9c833d178b4ee0d2648797fdfe6ccf5
kaushik-evani <kaushik.evani@gmail.com>
2018-02-24 16:50:42 +0530

    1st version of the site rolling out

---
+++ blinker.js
@@ -0,0 +1,29 @@
+$(function(){
+    var opened = '+_+';
+    var closed = '-_-';
+
+    var theX = $('#theX');
+    
+    function close() {
+        theX.text(closed);
+    }
+
+    function open() {
+        theX.text(opened);
+        waitRandAndBlink()
+    }
+    
+    function blink() {
+        close();
+        var rand =  Math.floor(Math.random() * (300 - 50 + 1)) + 50;
+        setTimeout(open, rand);
+    }
+
+    function waitRandAndBlink() {
+        var rand = Math.round(Math.random() * 10) * 500
+        console.log(rand);
+        setTimeout(blink, rand);
+    }
+
+    waitRandAndBlink();
+ });
